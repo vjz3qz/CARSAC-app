33d3b88bbb93d7743d7d6bebae58a6d3c6321f47
vjz3qz <vjz3qz@virginia.edu>
2023-05-25 17:39:59 -0400

    added server submodule

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = ('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(3000, () => {
-	console.log('Server running on port 3000');
-});
